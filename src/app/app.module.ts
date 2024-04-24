a85cf853ab657053019c646c750a115681e781d4
ZlabiDev <zlabi.dev@gmail.com>
2024-04-24 22:02:18 +0200

    first tutorial zwischenstand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {AuthContentComponent} from './components/auth-content/auth-content.component';
+import {WelcomeContentComponent} from './components/welcome-content/welcome-content.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
+import {ContentComponent} from './components/content/content.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        AuthContentComponent
+        AuthContentComponent,
+        WelcomeContentComponent,
+        LoginFormComponent,
+        ContentComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
